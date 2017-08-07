6636e9518e6dc1a34785d254a0c0a33fcdcbb779
李文浩 <lwhtarena@163.com>
2017-08-08 00:10:03 +0800

    一点一点积累

---
+++ 11 RxJS教程/base/base.js
@@ -0,0 +1,33 @@
+/**
+ * 基础：
+ *  数据转换成observables
+ */
+
+//来自一个或多个值
+Rx.Observable.of('foo','bar');
+
+//来自数组
+Rx.Observable.from([1,2,3]);
+
+//来自事件
+Rx.Observable.fromElement(document.querySelector('button'),'click');
+
+//来自Promise
+Rx.Observable.fromPromise(fetch('/users'));
+
+// 来自回调函数(最后一个参数得是回调函数，比如下面的 cb)
+// fs.exists = (path, cb(exists))
+var exists = Rx.Observable.bindCallback(fs.exists);
+exists('file.txt').subscribe(exists => console.log('Does file exist?', exists));
+
+// 来自回调函数(最后一个参数得是回调函数，比如下面的 cb)
+// fs.rename = (pathA, pathB, cb(err, result))
+var rename = Rx.Observable.bindNodeCallback(fs.rename);
+rename('file.txt', 'else.txt').subscribe(() => console.log('Renamed!'));
+
+
+
+
+
+
+
