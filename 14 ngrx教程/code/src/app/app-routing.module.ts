fdde06574377367e661fba101d088f7865ac5266
2H6YTERK4NWOTAC\Administrator <lwhtarena@163.com>
2019-09-02 00:07:19 +0800

    example

--- 14 ngrx教程/code/src/app/app-routing.module.ts
+++ 14 ngrx教程/code/src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from '../app/auth/services';
+import { NotFoundPageComponent } from './core/containers';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/books', pathMatch: 'full' },
+  {
+    path: 'books',
+    loadChildren: () => import('./books/books.module').then(m => m.BooksModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+    data: { title: 'Not found' },
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ 14 ngrx教程/docs/readme.md
@@ -0,0 +1,7 @@
+# ngrx 
+
+![](./img/001.png)
+
+## ngrx/store
+
+![](./img/state-management-lifecycle.png)
