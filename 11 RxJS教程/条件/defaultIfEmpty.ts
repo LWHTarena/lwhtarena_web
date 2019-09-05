2dac2e778de5085a928c56a492519c8f1f2399a2
2H6YTERK4NWOTAC\Administrator <lwhtarena@163.com>
2019-09-05 22:09:31 +0800

    条件

--- 11 RxJS教程/条件/defaultIfEmpty.ts
+++
@@ -1,8 +0,0 @@
-// RxJS v6+
-import { defaultIfEmpty } from 'rxjs/operators';
-import { empty } from 'rxjs';
-
-// 当源 observable 为空时，发出 'Observable.empty()!'，否则发出源的任意值
-const example = empty().pipe(defaultIfEmpty('Observable.empty()!'));
-// 输出: 'Observable.empty()!'
-const subscribe = example.subscribe(val => console.log(val));
--- 11 RxJS教程/条件/defaultIfOf.ts
+++
@@ -1,8 +0,0 @@
-// RxJS v6+
-import { defaultIfEmpty } from 'rxjs/operators';
-import { of } from 'rxjs';
-
-// 当源 observable 为空时，发出 'Observable.of() Empty!'，否则发出源的任意值
-const exampleOne = of().pipe(defaultIfEmpty('Observable.of() Empty!'));
-// 输出: 'Observable.of() Empty!'
-const subscribe = exampleOne.subscribe(val => console.log(val));
