10de6cbf9efc9c1796e705bc37c4e79413829103
李文浩 <yuanyuanliwenhao@163.com>
2019-10-22 09:27:03 +0800

    root 模块

--- 14 ngrx教程/code/src/app/app.module.ts
+++ 14 ngrx教程/code/src/app/app.module.ts
@@ -33,6 +33,10 @@ import { AppComponent } from '../app/core/containers';
      * reducers, combineReducers will be run creating your application
      * meta-reducer. This returns all providers for an @ngrx/store
      * based application.
+     *
+     * StoreModule.forRoot在根模块中一次导入，接受一个reducer函数或reducer函数的对象映射。
+     * 如果传递了reducers对象，则会运行 combineReducers 创建您的应用程序meta-reducer。这将
+     * 返回基于 @ngrx/store 的应用程序的所有提供程序。
      */
     StoreModule.forRoot(ROOT_REDUCERS, {
       metaReducers,
@@ -46,6 +50,7 @@ import { AppComponent } from '../app/core/containers';
 
     /**
      * @ngrx/router-store keeps router state up-to-date in the store.
+     * @ngrx/router-store使路由器状态在存储中保持最新。
      */
     StoreRouterConnectingModule.forRoot({
       routerState: RouterState.Minimal,
@@ -55,6 +60,7 @@ import { AppComponent } from '../app/core/containers';
      * Store devtools instrument the store retaining past versions of state
      * and recalculating new states. This enables powerful time-travel
      * debugging.
+     * Store devtools对store进行检测，以保留状态的过去版本并重新计算新状态。这样可以进行强大的时间旅行调试。
      *
      * To use the debugger, install the Redux Devtools extension for either
      * Chrome or Firefox
@@ -72,6 +78,7 @@ import { AppComponent } from '../app/core/containers';
      * EffectsModule.forRoot() is imported once in the root module and
      * sets up the effects class to be initialized immediately when the
      * application starts.
+     * EffectsModule.forRoot（）在根模块中导入一次，并将effects class设置为在应用程序启动时立即初始化。
      *
      * See: https://ngrx.io/guide/effects#registering-root-effects
      */
